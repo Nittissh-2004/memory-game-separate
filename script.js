daf3c3fbda8f0d4eb71e97ef526fb65c335ee4cd
Nittissh-2004 <nittissh2004@gmail.com>
2025-05-05 12:47:34 +0530

    updated

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ const cardImages = [
   'images/img3.png',
   'images/img4.png',
   'images/img5.png',
-  'images/img6.png'
+  'images/img6.png',
+  'images/img7.png',
+  'images/img8.png',
+  'images/img9.png',
+  'images/img10.png',
+  'images/img11.png',
+  'images/img12.png'
 ];
 let cardsArray = [...cardImages, ...cardImages];
 let firstCard, secondCard;
